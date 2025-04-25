2fdd03273b32b980787b669f513fd8c11f54b912
ALVARADO <alvarado@ejemplo.com>
2025-04-25 18:52:50 -0500

    Add Axios for API requests and implement form for employee data submission

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
+
+  const [nombre, setNombre] = useState("");
+  const [edad, setEdad] = useState("0");
+  const [pais, setPais] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [correo, setCorreo] = useState("");
+
+  const add = () => {
+    Axios.post("http://localhost:3001/create", {
+      nombre: nombre,
+      edad: edad,
+      pais: pais,
+      cargo: cargo,
+      correo: correo
+    }).then((response) => {
+      alert("Empleado agregado");
+      console.log(response);
+    });
+  }
+
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <div className="datos">
+          <label> Nombre: 
+            <input onChange={(event) => {setNombre(event.target.value)}} type="text"/>
+          </label>
+          <label> Edad: 
+            <input onChange={(event) => {setEdad(event.target.value)}} type="number"/>
+          </label>
+          <label> País: 
+            <input onChange={(event) => {setPais(event.target.value)}} type="text"/> 
+          </label>
+          <label> Cargo: 
+            <input onChange={(event) => {setCargo(event.target.value)}} type="text"/>
+          </label>
+          <label> Correo: 
+            <input onChange={(event) => {setCorreo(event.target.value)}} type="text"/>
+          </label>
+          <button onClick={add}>Agregar</button>
+      </div>
     </div>
   );
 }
